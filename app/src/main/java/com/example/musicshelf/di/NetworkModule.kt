1e643a2a4f87d99c2dfe021f99fc6dcb361d2600
Sounak <sounakchakraborty371@gmail.com>
2026-03-10 07:55:10 +0530

    Bypass SSL certificate validation for Spotify API debugging

--- app/src/main/java/com/example/musicshelf/di/NetworkModule.kt
+++ app/src/main/java/com/example/musicshelf/di/NetworkModule.kt
@@ -19,6 +19,11 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import javax.net.ssl.HostnameVerifier
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -35,9 +40,27 @@ object NetworkModule {
     fun provideOkHttpClient(
         spotifyTokenInterceptor: SpotifyTokenInterceptor
     ): OkHttpClient {
-        return OkHttpClient.Builder()
-            .addInterceptor(spotifyTokenInterceptor)
-            .build()
+        return try {
+            val trustAllCerts = arrayOf<TrustManager>(
+                object : X509TrustManager {
+                    override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                    override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                }
+            )
+
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+            val sslSocketFactory = sslContext.socketFactory
+
+            OkHttpClient.Builder()
+                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                .hostnameVerifier(HostnameVerifier { _, _ -> true })
+                .addInterceptor(spotifyTokenInterceptor)
+                .build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
     }
 
     @Provides
